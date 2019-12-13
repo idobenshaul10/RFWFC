2b892f75b39f932ffaac4a76f548c99fae5d746e
idobenshaul10 <ido.benshaul@gmail.com>
2019-12-13 19:06:08 +0200

    k-fold alpha smoothness.

--- utils.py
+++ utils.py
@@ -69,6 +69,49 @@ def predict_model(x, model, method='RF', m=10):
     else:
         raise Exception('Method incorrect - should be either RF or WF')
 
+def kfold_alpha_smoothness(x, y, t_method='RF', num_wavelets=10, n_folds=10, n_trees=5, m_depth=9,
+                         n_features='auto', n_state=2000, normalize=True, norm_normalization='volume'):
+    # Input - Labeled data and number of folds
+    # Output - Mean and standard deviation of mean squared errors over all folds
+
+    # Normalize the data if needed
+    if normalize:
+        x = normalize_data(x)
+
+    # Use scikit-learn's KFold, will automatically split the data into training and testing in each fold
+    kf = KFold(n_splits=n_folds)
+    alphas = []
+
+    # Shuffle the data indexes to get k-random folds
+    np.random.seed(seed=n_state)
+    shuffle_data = np.arange(len(x))
+    np.random.shuffle(shuffle_data)
+
+    norm_m_term = 0
+
+    for train, test in kf.split(x):
+        # Create the training and testing arrays for each fold
+        x_train = x[shuffle_data[train]]
+        y_train = y[shuffle_data[train]]
+        x_test = x[shuffle_data[test]]
+        y_test = y[shuffle_data[test]]
+        model = train_model(x_train, y_train, method=t_method, trees=n_trees,
+                    depth=m_depth, features=n_features, state=n_state, nnormalization=norm_normalization)
+        if t_method == 'WF':
+            if num_wavelets < 1:
+                num_wavelets = int(np.round(num_wavelets*len(model.norms)))
+                norm_m_term = -np.sort(-model.norms)[num_wavelets-1]
+
+        alpha, n_wavelets, errors = model.evaluate_smoothness(m=num_wavelets)
+        # Calculate the MSE accuracy and append it to the accuracies vector
+        # mse.append(metrics.mean_squared_error(y_test, y_pred))
+        alphas.append(alpha)
+        logging.log(20, ' Fold alpha: '+str(alphas[-1]))
+
+    logging.log(60, 'Mean of MSE over all folds: ' + str(np.mean(alphas)) +
+                '   Standard deviation: ' + str(np.std(alphas)))
+    return np.mean(alphas), np.std(alphas), num_wavelets, norm_m_term
+
 
 def kfold_regression_mse(x, y, t_method='RF', num_wavelets=10, n_folds=10, n_trees=5, m_depth=9,
                          n_features='auto', n_state=2000, normalize=True, norm_normalization='volume'):
@@ -115,16 +158,16 @@ def find_m_term(x, y, budget=100, folds=10, trees=5, depth=9, features='auto', s
                 nnormalization='volume'):
     mse_m = np.zeros((budget, 4))
     for k in range(0, budget):
-        logging.log(60, '   Using ' + method + ' in ' + str(k) + ' iteration.')
+        logging.log(60, ' Using ' + method + ' in ' + str(k) + ' iteration.')
         if method == 'hop':
             wavelets = (k+1)/budget - np.finfo(float).eps
         else:
             wavelets = k+1
         mse_m[k, 0], mse_m[k, 1], mse_m[k, 2], mse_m[k, 3] = kfold_regression_mse(x, y, t_method='WF',
-                                                                                  num_wavelets=wavelets, n_folds=folds,
-                                                                                  n_trees=trees, m_depth=depth,
-                                                                                  n_features=features, n_state=state,
-                                                                                  norm_normalization=nnormalization)
+                                                          num_wavelets=wavelets, n_folds=folds,
+                                                          n_trees=trees, m_depth=depth,
+                                                          n_features=features, n_state=state,
+                                                          norm_normalization=nnormalization)
     return mse_m, int(mse_m[np.argmin(mse_m[:, 0]), 2]), mse_m[np.argmin(mse_m[:, 0]), 3]
 
 
