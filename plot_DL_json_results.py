05c5e11732a629d6c565b0d76370fe2683da6efa
Your Name <you@example.com>
2020-08-21 17:44:58 +0300

    added test top 1 acc to plotting script

--- plot_DL_json_results.py
+++ plot_DL_json_results.py
@@ -15,21 +15,24 @@ import json
 main_dir = sys.argv[1]
 # paths = Path('src').rglob('*.json')
 # indices = [5, 10, 20, 30]
+fig, axes = plt.subplots(1, 2)
 for file_path in Path(main_dir).glob('**/*.json'):	
 	file_path = str(file_path)	
 	epoch = file_path.split('\\')[-2].split('.')[-2]
-	eps = file_path.split('\\')[-2].split('.')[1]
-	# if eps != '05_0':
-	# 	continue
-	# if int(epoch)%5 != 0:
-	# 	continue
+	eps = file_path.split('\\')[-2].split('.')[1]	
 	with open(file_path, "r+") as f:
 		result = json.load(f)	
 	sizes = result["sizes"]
 	alphas = result["alphas"]
+
+	test_stats = None
+	if 'test_stats' in result:
+		test_stats = result['test_stats']		
 	# plt.fill_between(sizes, [k[0] for k in alphas], [k[1] for k in alphas], \
 	# 	alpha=0.2, linewidth=4)
-	plt.plot(sizes, [np.array(k).mean()	 for k in alphas], label=f"{epoch}")
+	axes[0].plot(sizes, [np.array(k).mean()	 for k in alphas], label=f"{epoch}")
+	if test_stats is not None:
+		axes[1].scatter(epoch, [test_stats['top_1_accuracy']], label=f"{epoch}")
 
 plt.legend()
 plt.title("alphas for different epochs")
