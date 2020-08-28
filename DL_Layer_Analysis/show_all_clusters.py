95ae1921f0a71ed1ae068fd72f007c36cabac31a
Your Name <you@example.com>
2020-08-28 23:18:28 +0300

    plotting all layers

--- DL_Layer_Analysis/show_all_clusters.py
+++ DL_Layer_Analysis/show_all_clusters.py
@@ -21,24 +21,37 @@ def get_args():
 	args = parser.parse_args()
 	return args
 
-def plot_clusters(main_dir):	
-	fig=plt.figure()
-	file_paths = glob.glob(os.path.join(main_dir, "*.png"))		
-	file_paths = [k for k in file_paths if '_' not in k.split('\\')[-1]]
-	width, height =224, 224
-	rows = 1
-	cols = len(file_paths)
-	axes=[]
-
-	for idx, file in enumerate(file_paths):	    
-	    b = cv2.imread(file)	    
-	    axes.append( fig.add_subplot(rows, cols, idx+1) )
-	    # subplot_title=("Subplot"+str(idx))
-	    # axes[-1].set_title(subplot_title)  
-	    plt.imshow(b)
-	fig.tight_layout()    
+def show_images(images, cols = 1, titles = None):
+	"""Display a list of images in a single figure with matplotlib.
+	
+	Parameters
+	---------
+	images: List of np.arrays compatible with plt.imshow.
+	
+	cols (Default = 1): Number of columns in figure (number of rows is 
+						set to np.ceil(n_images/float(cols))).
+	
+	titles: List of titles corresponding to each image. Must have
+			the same length as titles.
+	"""
+	assert((titles is None) or (len(images) == len(titles)))
+	n_images = len(images)
+	if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+	fig = plt.figure()
+	for n, (image, title) in enumerate(zip(images, titles)):
+		a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+		if image.ndim == 2:
+			plt.gray()
+		plt.imshow(image)
+		a.set_title(title)
+	fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
 	plt.show()
 
 if __name__ == '__main__':
 	args = get_args()	
-	plot_clusters(args.main_dir)
+	file_paths = glob.glob(os.path.join(args.main_dir, "*.png"))
+	file_paths = [k for k in file_paths if '_' not in k.split('\\')[-1]]
+	file_paths = [k for k in file_paths if 'result' not in k.split('\\')[-1]]
+	images = [cv2.imread(file) for file in file_paths]
+	show_images(images)
+	# plot_clusters(args.main_dir)
